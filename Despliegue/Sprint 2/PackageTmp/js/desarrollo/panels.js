f6c8047376d18f9ebf55a40ca8e653b8c49c65aa
florenciarojasamaya <florenciarojasamaya@gmail.com>
2014-06-11 02:52:09 +0000

    Quito cosas de agiles del repo, salvo codigo

--- Despliegue/Sprint 2/PackageTmp/js/desarrollo/panels.js
+++
@@ -1,9 +0,0 @@
-﻿function ocultarPaneles() {
-    if (Page_ClientValidate()) {
-
-        document.getElementById("panelFracaso").style.display = 'none';
-    } else {
-
-        document.getElementById("panelExito").style.display = 'none';
-    }
-}
