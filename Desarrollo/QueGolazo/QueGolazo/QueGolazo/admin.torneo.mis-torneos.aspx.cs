851b910a7db31ccccf9d5f35abd0b0526bf2d7c3
antonioherrera990 <antonioherrera990@9ade7d50-6d57-6798-a288-c19d408c4fea>
2014-06-24 19:26:52 +0000

    Creacion de codigo para registrar campeonato.

--- Desarrollo/QueGolazo/QueGolazo/QueGolazo/admin.torneo.mis-torneos.aspx.cs
+++ Desarrollo/QueGolazo/QueGolazo/QueGolazo/admin.torneo.mis-torneos.aspx.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Utils;
+using Entidades;
 namespace QueGolazo
 {
     public partial class admin_torneo_mis_torneos : System.Web.UI.Page
@@ -13,5 +14,25 @@ namespace QueGolazo
         {
 
         }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Usuario usuarioActual = (Usuario)Session["usuario"];
+            if (validaDatosDeEntrada()) { 
+            
+
+            }
+        }
+
+        /// <summary>
+        /// Valida si los datos introducidos del lado del cliente son válidos.
+        /// <returns>True si son validos los datos, False si son inválidos.</returns>
+        private bool validaDatosDeEntrada()
+        {
+            ValidacionDeTextos validador = new ValidacionDeTextos();
+            return !validador.estaVacio(txtNickTorneo.Value) && !validador.estaVacio(txtNombreTorneo.Value);
+
+        }
+
     }
 }
--- Desarrollo/QueGolazo/QueGolazo/QueGolazo/admin.torneo.mis-torneos.aspx.designer.cs
+++ Desarrollo/QueGolazo/QueGolazo/QueGolazo/admin.torneo.mis-torneos.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace QueGolazo {
     
     
     public partial class admin_torneo_mis_torneos {
+        
+        /// <summary>
+        /// Control frmRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm frmRegistrar;
+        
+        /// <summary>
+        /// Control txtNombreTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtNombreTorneo;
+        
+        /// <summary>
+        /// Control txtNickTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtNickTorneo;
+        
+        /// <summary>
+        /// Control txtDescripcionTorneo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea txtDescripcionTorneo;
+        
+        /// <summary>
+        /// Control exampleInputFile.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile exampleInputFile;
+        
+        /// <summary>
+        /// Control btnRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrar;
     }
 }
