5e542032541f928c72d523b310f00421a05c6188
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@9ade7d50-6d57-6798-a288-c19d408c4fea>
2014-06-24 18:32:19 +0000

    CLASE USUARIO

--- trunk/Desarrollo/QueGolazo/QueGolazo/Entidades/Usuario.cs
+++ trunk/Desarrollo/QueGolazo/QueGolazo/Entidades/Usuario.cs
@@ -9,9 +9,13 @@ namespace Entidades
     public class Usuario
     {
         public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
         public string email { get; set; }
+        public string telefono { get; set; }
         public string contrasenia { get; set; }
-        public TipoUsuario rol { get; set; }
-        public Estado estado { get; set; }
+        public bool esActivo { get; set; }
+        public string codigo { get; set; }
+        public TipoUsuario tipoUsuario { get; set; }
     }
 }
