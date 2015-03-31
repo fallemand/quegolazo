3f385addfcfef55143f6fa6c725e7484b75d91a8
facualle@gmail.com <facualle@gmail.com@9ade7d50-6d57-6798-a288-c19d408c4fea>
2015-03-31 21:34:39 +0000

    -Correcciones Responsive.

--- trunk/Interfaz de Usuario/Torneo Nuevo/torneo/js/main.js
+++ trunk/Interfaz de Usuario/Torneo Nuevo/torneo/js/main.js
@@ -32,65 +32,6 @@ $(document).ready(function($) {
     },
     itemTemplate: '<li><a href="{{image_b}}" class="fancybox"><img src="{{image_s}}" alt="{{title}}" /></a></li>'
   });
-
-  //=================================== Google Map  ==============================//
-  
-  /*
-    Map Settings
-    Find the Latitude and Longitude of your address:  
-    - http://universimmedia.pagesperso-orange.fr/geo/loc.htm  
-    - http://www.findlatitudeandlongitude.com/find-address-from-latitude-and-longitude/
-  */
-
-  // Map Markers
-  var mapMarkers = [{
-    address: "217 Summit Boulevard, Birmingham, AL 35243",
-    html: "<strong>Alabama Office</strong><br>217 Summit Boulevard, Birmingham, AL 35243<br><br><a href='#' onclick='mapCenterAt({latitude: 33.44792, longitude: -86.72963, zoom: 16}, event)'>[+] zoom here</a>",
-    icon: {
-      image: "img/img-theme/pin.png",
-      iconsize: [26, 46],
-      iconanchor: [12, 46]
-    }
-  },{
-    address: "645 E. Shaw Avenue, Fresno, CA 93710",
-    html: "<strong>California Office</strong><br>645 E. Shaw Avenue, Fresno, CA 93710<br><br><a href='#' onclick='mapCenterAt({latitude: 36.80948, longitude: -119.77598, zoom: 16}, event)'>[+] zoom here</a>",
-    icon: {
-      image: "img/img-theme/pin.png",
-      iconsize: [26, 46],
-      iconanchor: [12, 46]
-    }
-  },{
-    address: "New York, NY 10017",
-    html: "<strong>New York Office</strong><br>New York, NY 10017<br><br><a href='#' onclick='mapCenterAt({latitude: 40.75198, longitude: -73.96978, zoom: 16}, event)'>[+] zoom here</a>",
-    icon: {
-      image: "img/img-theme/pin.png",
-      iconsize: [26, 46],
-      iconanchor: [12, 46]
-    }
-  }];
-
-  // Map Initial Location
-  var initLatitude = 38.09024;
-  var initLongitude = -98.71289;
-
-  // Map Extended Settings
-  var mapSettings = {
-    controls: {
-      panControl: true,
-      zoomControl: true,
-      mapTypeControl: true,
-      scaleControl: true,
-      streetViewControl: true,
-      overviewMapControl: true
-    },
-    scrollwheel: false,
-    markers: mapMarkers,
-    latitude: initLatitude,
-    longitude: initLongitude,
-    zoom: 5
-  };
-  
-  $("#map").gMap(mapSettings);
  
 	//=================================== Nav Responsive =============================//
 
