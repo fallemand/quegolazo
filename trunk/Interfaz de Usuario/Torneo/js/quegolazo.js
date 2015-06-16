da6765701b85d3528f722bcd8d290b30aae04a8d
facualle@gmail.com <facualle@gmail.com@9ade7d50-6d57-6798-a288-c19d408c4fea>
2015-06-16 02:19:55 +0000

    -Presentación final y checklist

--- trunk/Interfaz de Usuario/Torneo/js/quegolazo.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
-
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if($(e.target).is('a')) {
-        $(this).collapse('hide');
-    }
-});
